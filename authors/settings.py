5c13d67a4260a0739a17751608188744b7119ebf
kasulejoseph <kasule06joseph@gmail.com>
2018-12-04 17:30:00 +0300

    feat(jwt auth): implement jwt authentication
    return a token upon successful login
    allow a user to access authenticated  endpoints with the generated token
    move SECRET_KEY from settings.py to .env
    
    [Delivers #162163262]

--- authors/settings.py
+++ authors/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY", None)
+SECRET_KEY = os.getenv("SECRET_KEY", '123456789')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", False)
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
